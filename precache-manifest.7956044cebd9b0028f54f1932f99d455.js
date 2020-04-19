075cbcd2b399fd4511399a0446029c18148bce40
WojtekBoman <wojciechboman@gmail.com>
2020-04-19 13:22:46 +0200

    Updates

---
+++ precache-manifest.7956044cebd9b0028f54f1932f99d455.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63489f46d1e5902490f716f8be8cd5d0",
+    "url": "/VolunteerGroupFront/index.html"
+  },
+  {
+    "revision": "a24b9d5a0356c80cc382",
+    "url": "/VolunteerGroupFront/static/css/2.de424728.chunk.css"
+  },
+  {
+    "revision": "611c58837973577a7f4f",
+    "url": "/VolunteerGroupFront/static/css/main.77c44a82.chunk.css"
+  },
+  {
+    "revision": "a24b9d5a0356c80cc382",
+    "url": "/VolunteerGroupFront/static/js/2.931a6bfa.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/VolunteerGroupFront/static/js/2.931a6bfa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "611c58837973577a7f4f",
+    "url": "/VolunteerGroupFront/static/js/main.7f4d63a7.chunk.js"
+  },
+  {
+    "revision": "142cbb8cb31c12fef9d9",
+    "url": "/VolunteerGroupFront/static/js/runtime-main.3e0f75f9.js"
+  },
+  {
+    "revision": "de3a50ffae5e099002d2bcc3273246ba",
+    "url": "/VolunteerGroupFront/static/media/background1.de3a50ff.jpg"
+  },
+  {
+    "revision": "303269b8c6650c229994ca65e4d400e3",
+    "url": "/VolunteerGroupFront/static/media/background2.303269b8.jpg"
+  },
+  {
+    "revision": "9d42dd43a46c1445850a5da13f0f9e3f",
+    "url": "/VolunteerGroupFront/static/media/background3.9d42dd43.jpg"
+  }
+]);
