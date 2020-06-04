018b51f5478cddd84e0cbdcaab01bef55664be58
WojtekBoman <wojciechboman@gmail.com>
2020-06-04 18:18:19 +0200

    Hide statistics for volunteers and workers

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -106,6 +106,13 @@ class Navbar extends React.Component{
                     </li>
                   </Link>
                   <li class="nav-item dropdown">
+                  <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown">Statystyki</a>
+                  <div class="dropdown-menu">
+                        <Link to={"/wykresAktywnosci"} className="dropdown-item">Aktywność</Link>
+                        <Link to={"/wykresWydarzen"} className="dropdown-item">Zbiórki</Link>
+                    </div>
+                  </li>
+                  <li class="nav-item dropdown">
                   <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown">Wydarzenia</a>
                   <div class="dropdown-menu">
                         <Link to={"/wydarzenia"} className="dropdown-item">Przeglądaj</Link>
@@ -156,13 +163,7 @@ class Navbar extends React.Component{
                         <Link to={"/nowaWiadomosc"} className="dropdown-item">Wyślij wiadomość</Link>
                     </div>
                   </li>
-                  <li class="nav-item dropdown">
-                  <a href="#" className="nav-link dropdown-toggle" data-toggle="dropdown">Statystyki</a>
-                  <div class="dropdown-menu">
-                        <Link to={"/wykresAktywnosci"} className="dropdown-item">Aktywność</Link>
-                        <Link to={"/wykresWydarzen"} className="dropdown-item">Zbiórki</Link>
-                    </div>
-                  </li>
+                  
                 <Link to={"/userEvents"} className="nav-link">
                   <li className="nav-item">
                     Twoje wydarzenia
